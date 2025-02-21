3739b29bfac1e685e855581fa8564d1d9bd3fa34
linji1 <saboha@163.com>
2025-02-21 23:51:44 +0800

    同步

--- docs/pluging/ArticleJs.js
+++ docs/pluging/ArticleJs.js
@@ -136,7 +136,7 @@ document.addEventListener('DOMContentLoaded', () => {
 	//////////////// 引入fancybox所需的css文件以及所需的绑定函数 start ////////////////
 	document.head.appendChild(Object.assign(document.createElement('link'), {
 		rel: 'stylesheet',
-		href: 'https://cdn.jsdelivr.net/npm/@fancyapps/ui@5.0/dist/fancybox/fancybox.css'
+		href: '/fancyapps/dist/fancybox/fancybox.css'
 	}));
 	Fancybox.bind('[data-fancybox="gallery"]', {});
 	//////////////// 引入fancybox所需的css文件以及所需的绑定函数 end ////////////////
