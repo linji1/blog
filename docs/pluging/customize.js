b1f1743a020cd5e3e953f3671561ebc3dfb19bd8
linji1 <saboha@163.com>
2025-02-24 13:07:46 +0800

    同步

--- docs/pluging/customize.js
+++ docs/pluging/customize.js
@@ -81,6 +81,19 @@ body {
 .markdown-body img {
     border-radius: 10px;	
 }
+
+@keyframes waveMove {
+	from {background-position-x: 0;}
+	to {background-position-x: 50%;}
+}
+
+a:hover {
+  text-decoration: none;
+  background: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 4'%3E%3Cpath fill='none' stroke='%23333' d='M0 3.5c5 0 5-3 10-3s5 3 10 3 5-3 10-3 5 3 10 3'/%3E%3C/svg%3E") repeat-x 0 100%;
+  background-size: 20px auto;
+  animation: waveMove 1s infinite linear;
+}
+
 /* 网站导航开始 */
    .core-next-dh-shortcode {
     display: inline-block;
